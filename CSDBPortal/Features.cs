8b44e1f819454b1270daec3bf9015b298efa77b7
MNC Deepthi <bvsaisrideep@yahoo.com>
2023-03-12 21:57:15 +0530

    missing file added

---
+++ CSDBPortal/Features.cs
@@ -0,0 +1,20 @@
+﻿namespace CSDBPortal
+{
+    public static class Features
+    {
+        public static string IssuAdministration = "Issue Administration";
+        public static string InfoCodeSetAdministration = "Info Code Set Administration";
+        public static string InfoCodeAdministration = "Info Code Administration";
+        public static string IcnFormatAdministration = "ICN Format Administration";
+        public static string LocationCodeSetAdministration = "Location Code Set Administration";
+        public static string LocationCodeAdministration = "Location Code Administration";
+        public static string RPCAdministration = "RPC Administration";
+        public static string DataModuleTypeAdministration = "Data Module Type Administration";
+        public static string SNSAdministration = "SNS Administration";
+        public static string ProjectAdministration = "Project Administration";
+        public static string DMCAdministration = "DMC Administration";
+        public static string GenerateICNNumber = "Generate ICN Number";
+        public static string ViewICNNumber = "View ICN Number";
+        public static string UserAndRoleAdministration = "User and Role Administration";
+    }
+}
