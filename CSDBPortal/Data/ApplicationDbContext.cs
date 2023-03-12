15c67804092b05e3c20659cbb2f9689191d1d910
MNC Deepthi <bvsaisrideep@yahoo.com>
2023-03-12 21:53:00 +0530

    build errors

--- CSDBPortal/Data/ApplicationDbContext.cs
+++ CSDBPortal/Data/ApplicationDbContext.cs
@@ -41,6 +41,8 @@ namespace CSDBPortal.Data
         public virtual DbSet<UserDetail> UserDetails { get; set; } = null!;
         public virtual DbSet<XmlValidation> XmlValidations { get; set; } = null!;
 
+        public virtual DbSet<DataModuleCode> DataModuleCodes { get; set; } = null;
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
